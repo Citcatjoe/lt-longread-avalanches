2891de99ffaf75fe689040646ffd3e128106fa68
César Greppin <cesar.greppin@ringier.ch>
2018-01-25 17:22:59 +0100

    day2

--- js/header.js
+++ js/header.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($)
 {
 	var $headerBgImg = $('.header--bg--img');
 	var $headerBgVid = $('.header--bg--vid');
+	var $headerTitle = $('.header--title');
 	var controller = new ScrollMagic.Controller({});
 
 	setIntroDone();
@@ -27,8 +28,11 @@ jQuery(document).ready(function($)
 	tlIntroduction =  new TimelineMax({ paused: false });
 
 	tlIntroduction
+		.set($headerTitle, { autoAlpha: 0, yPercent: '10' })
 		.to([$spinner, $logoLt], 0.7, {autoAlpha: 0, ease: Power4.easeOut}, '+=1.5')
-		.to($overlay, 1, {autoAlpha: 0, ease:Linear.easeNone}, '-=0.0');
+		.to($overlay, 1, {autoAlpha: 0, ease:Linear.easeNone}, '-=0.0')
+		.to($headerTitle, 2, { autoAlpha: 1, yPercent: '0' }, '+=2.0')
+		.set($headerTitle, { clearProps: "all" }); ;
 
 
 
